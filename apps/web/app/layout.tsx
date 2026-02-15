27bec7110485d154638b23cd720c1ea5fd03cbd7
robin <nowrobin3@gmail.com>
2026-02-15 21:48:15 +0900

    change analytics script to  package

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata, Viewport } from 'next'
-import Script from 'next/script'
 import { cn } from '@/lib/utils'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/next'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import mixpanel from 'mixpanel'
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID || ''
 const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN || ''
@@ -37,6 +38,8 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode
 }>) => {
+  //필요한 기능 추후 추가
+  const mp = mixpanel.init(MIXPANEL_TOKEN)
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
@@ -49,31 +52,9 @@ const RootLayout = ({
         />
         {/* PWA iOS 지원 */}
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-
-        {/* Mixpanel */}
-        <Script id="mixpanel-sdk" strategy="afterInteractive">
-          {`
-            (function(f,b){if(!b.__SV){var e,g,i,h;window.mixpanel=b;b._i=[];b.init=function(e,f,c){function g(a,d){var b=d.split(".");2==b.length&&(a=a[b[0]],d=b[1]);a[d]=function(){a.push([d].concat(Array.prototype.slice.call(arguments,0)))}}var a=b;"undefined"!==typeof c?a=b[c]=[]:c="mixpanel";a.people=a.people||[];a.toString=function(a){var d="mixpanel";"mixpanel"!==c&&(d+="."+c);a||(d+=" (stub)");return d};a.people.toString=function(){return a.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");for(h=0;h<i.length;h++)g(a,i[h]);var j="set set_once union unset remove delete".split(" ");a.get_group=function(){function b(c){d[c]=function(){call2_args=arguments;call2_args=[c].concat(Array.prototype.slice.call(call2_args,0));a.push([e,call2_args])}}for(var d={},e=["get_group"].concat(Array.prototype.slice.call(arguments,0)),c=0;c<j.length;c++)b(j[c]);return d};b._i.push([e,f,c])};b.__SV=1.2;e=f.createElement("script");e.type="text/javascript";e.async=!0;e.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===f.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\\/\\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";g=f.getElementsByTagName("script")[0];g.parentNode.insertBefore(e,g)}})(document,window.mixpanel||[]);
-            mixpanel.init('${MIXPANEL_TOKEN}', {
-              autocapture: true,
-              record_sessions_percent: 0,
-            });
-          `}
-        </Script>
       </head>
       <body className={cn('min-h-screen bg-background antialiased')}>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_ID}');
-          `}
-        </Script>
+        <GoogleAnalytics gaId={GA_ID} />
         <Analytics />
         {children}
       </body>
