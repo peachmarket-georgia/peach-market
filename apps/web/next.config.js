edf0d2b324d1055ee4e145139271ada1ea5ce993
robin <nowrobin3@gmail.com>
2026-03-04 18:04:08 +0900

    Revert "web railway setting"
    This reverts commit b8c77a4bbdd1ce62c149d98b952a7ea3ff44f7b5.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,5 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../../'),
-  transpilePackages: ['@peachmarket/shared'],
   images: {
     remotePatterns: [
       {
